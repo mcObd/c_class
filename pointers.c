6b0770a2c6d0864f6a316941c35e3372d857cd3a
McQuendie <obodosmcquendie@gmail.com>
2025-09-11 15:07:46 +0100

    c

--- pointers.c
+++ pointers.c
@@ -6,6 +6,10 @@ int main(){
     int var1=2026, temp;
     int *var2;
     var2= &var1;
-    temp = *var2;
+    int *var3 = var2; //double pointer
+    printf("var2 = %d\n", *var2);
+    printf("var3 = %d\n", *var3);
+    // temp = *var2;
+    // printf("%d",temp);
     return 0;
 }
